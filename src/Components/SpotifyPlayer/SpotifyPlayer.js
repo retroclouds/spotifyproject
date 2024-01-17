f032ae63c6c6be28c39d31729d3a8fb7d462c5d1
retroclouds <115580725+retroclouds@users.noreply.github.com>
2024-01-17 14:34:20 +0000

    commenting out errors

--- src/Components/SpotifyPlayer/SpotifyPlayer.js
+++ src/Components/SpotifyPlayer/SpotifyPlayer.js
@@ -1,42 +1,66 @@
 import { useState} from "react";
 import { useEffect } from "react";
 
+
+
 export default function SpotifyPlayer({ accessToken, trackUri }) {
   console.log(accessToken);
 
+  /*
+
   const [currentSong, setCurrentSong] = useState(null);
 
-  /*
+  
   const sdkScript = document.createElement("script")
   sdkScript.src = "https://sdk.scdn.co/spotify-player.js"
   sdkScript.defer = true
 
   document.body.appendChild(sdkScript)
-  */
+  
 
-  useEffect(() => {
-    window.onSpotifyWebPlaybackSDKReady = () => {
-      const token = accessToken;
-      const player = new Spotify.Player({
+  window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = '[My access token]';
+    const player = new Spotify.Player({
         name: 'Web Playback SDK Quick Start Player',
         getOAuthToken: cb => { cb(token); },
         volume: 0.5
-      });
-    }
+    });
 
-    // Rest of your player setup code...
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+    });
+
+    player.addListener('initialization_error', ({ message }) => {
+        console.error(message);
+    });
+
+    player.addListener('authentication_error', ({ message }) => {
+        console.error(message);
+    });
+
+    player.addListener('account_error', ({ message }) => {
+        console.error(message);
+    });
+
+    document.getElementById('togglePlay').onclick = function() {
+      player.togglePlay();
+    };
 
     player.connect();
+}
 
-    // Clean up the player when the component is unmounted
-    return () => {
-      player.disconnect();
-    };
-  }, [accessToken]);
-
+*/
   return (
     <>
       {/* Your component JSX */}
     </>
   );
 }
+
+
